4182706469e16aca1263f487453c87218bd4f94f
AlanHenriqueRF <alhenrangelfera@gmail.com>
2023-04-26 10:57:04 -0300

    Versão com butão ativo e desativo

--- script/script.js
+++ script/script.js
@@ -1,27 +1,46 @@
-let nome = prompt('Qual o seu nome ?')
+let nome = prompt('Qual o seu nome ?');
+let link = '';
+axios.defaults.headers.common['Authorization'] = 'OdIdHp43nAdZeoW0W7UVQfKN';
 
 function selecionablusa(elemento){
-    const jaselecionado = document.querySelector('.modelo .selecionado')
+    const jaselecionado = document.querySelector('.modelo .selecionado');
     if (jaselecionado != null){
-        jaselecionado.classList.remove('selecionado')
+        jaselecionado.classList.remove('selecionado');
     }
-    elemento.classList.add('selecionado')
+    elemento.classList.add('selecionado');
+    ativandobutao();
 }
 
 function selecionagola(elemento){
-    const jaselecionado = document.querySelector('.gola .selecionado')
+    const jaselecionado = document.querySelector('.gola .selecionado');
     if (jaselecionado != null){
-        jaselecionado.classList.remove('selecionado')
+        jaselecionado.classList.remove('selecionado');
     }
-    elemento.classList.add('selecionado')
-
+    elemento.classList.add('selecionado');
+    ativandobutao();
 }
 
 function selecionatecido(elemento){
-    const jaselecionado = document.querySelector('.tecido .selecionado')
+    const jaselecionado = document.querySelector('.tecido .selecionado');
     if (jaselecionado != null){
-        jaselecionado.classList.remove('selecionado')
+        jaselecionado.classList.remove('selecionado');
     }
-    elemento.classList.add('selecionado')
+    elemento.classList.add('selecionado');
+    ativandobutao();
 }
 
+function verlink(elemento){
+    link = elemento.value;
+    ativandobutao();
+}
+
+function ativandobutao(){
+    if (document.querySelectorAll('.selecionado').length === 3 && (link != '' && link.slice(0,3)=== "htt")){
+        document.querySelector('button').classList.add('botaoativ');
+        document.querySelector('button').removeAttribute('disabled');
+    }
+    else {
+        document.querySelector('button').classList.remove('botaoativ');
+        document.querySelector('button').setAttribute('disabled','disabled')
+    }
+}
